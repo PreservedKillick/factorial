1940e6234503f950f7bef25173cb7b17f87b4bda
Angela Yen & Bailey Drake <student@epicodus.com>
2014-02-20 14:23:06 -0800

    factorial w. recursion

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,19 @@
 var factorial = function(number1) {
  var number = parseInt(number1);
- var result;
- var index = [];
- var input = number;
-
+ 
   if (number1 % 1 !== 0){
     return false;
-  } else if (number < 0) {
-    return false;
-  } else if ((number === 0) || (number === 1)) {
-     result = 1;
-     return result;
-  } else {
-    for (var i = (number-1); i > 0; i--) {
-    index.push(i);
-    } 
-    for (var n = 0; n<= (index.length - 1); n++) {
-    input = input * index[n];
-    result = input;
-    console.log(result);
+  
+    } else if (number < 0) {
+      return false;
+    } else if (number === 0) {
+      return 1;
+    } else {
+      return number * (factorial(number - 1));
     }
-  return result;
-  };
+  }; 
 
-};
+
 
 $(function(){
   $("form#factorial").submit(function(event){
