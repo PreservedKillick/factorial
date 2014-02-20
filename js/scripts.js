acccc2aeac2fa4ebe7b41d75f4c2f6bea0a5f75b
Angela Yen & Bailey Drake <student@epicodus.com>
2014-02-20 11:10:13 -0800

    successful factorial code

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 var factorial = function(number) {
  var result;
+ var index = [];
+ var input = number;
+
   if (number === 0) {
     result = 1;
   } else {
-    var digit = number;
-    result = 
+    for (var i = (number-1); i > 0; i--) {
+    index.push(i);
+    console.log(index);
+    } 
+    for (var n = 0; n<= (index.length - 1); n++) {
+    input = input * index[n];
+    result = input;
+    console.log(result);
+    };
+  return result;
   }
 
 return result;
